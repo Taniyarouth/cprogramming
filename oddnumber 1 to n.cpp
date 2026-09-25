47fb34cd455da8f29e44fd4cba03043e981a3b4d
Taniyarouth <taniyamishtu007@gmail.com>
2026-09-25 16:37:39 +0530

    Add files via upload

---
+++ oddnumber 1 to n.cpp
@@ -0,0 +1,13 @@
+//write a c program to display odd numbers from 1 to n 
+#include<stdio.h>
+int main()
+{
+	int n,i=1;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	printf("Odd numbers from 1 to %d\n",n);
+	while(i<=n){
+		printf("%d\n",i);
+		i=i+2;
+	}
+}
